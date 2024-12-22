7f1c4b7f448d605de1daf6285d3a47d8f9cdb496
khaledawad <kawad852@gmail.com>
2024-12-22 16:40:51 +0300

    menu back button

--- lib/screens/card/widgets/settings_screen.dart
+++ lib/screens/card/widgets/settings_screen.dart
@@ -26,6 +26,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(),
+      ),
       floatingActionButton: kDebugMode
           ? FloatingActionButton(
               onPressed: () {
